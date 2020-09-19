b24ed32da8c0a52d1110a218d669aec1802cb848
Avi Twito <avi.twito1988@gmail.com>
2020-09-19 20:00:58 +0300

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,103 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <v-app>
+      <div>
+        <v-app-bar>
+          <v-app-bar-nav-icon class="mx-1" @click="drawer = !drawer" ></v-app-bar-nav-icon>
+          
+          <v-toolbar-title v-if="$vuetify.breakpoint.lgAndUp">כותרת</v-toolbar-title>
+
+          <v-toolbar-items>
+            <v-btn text>לינק 1</v-btn>
+            <v-btn text>לינק 2</v-btn>
+            <v-btn text>לינק 3</v-btn>
+          </v-toolbar-items>
+
+          <v-spacer></v-spacer>
+
+          <!-- <template v-if="$vuetify.breakpoint.smAndUp">
+            <v-btn icon>
+              <v-icon>mdi-export-variant</v-icon>
+            </v-btn>
+            <v-btn icon>
+              <v-icon>mdi-delete-circle</v-icon>
+            </v-btn>
+            <v-btn icon>
+              <v-icon>mdi-plus-circle</v-icon>
+            </v-btn>
+          </template> -->
+        </v-app-bar>
+
+        <!-- <v-col cols="12" class="text-center">
+          <v-btn
+                class="primary mt-5"
+                @click="$vuetify.rtl = ! $vuetify.rtl"
+                >
+            Toglle RTL
+          </v-btn>
+        </v-col> -->
+
+        <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        :right="$vuetify.rtl"
+        temporary
+      >
+        <!-- <v-list-item>
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>John Leider</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item> -->
+
+        <v-divider></v-divider>
+
+        <v-list dense>
+
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-content :style="$vuetify.breakpoint.smAndDown ? 'padding-top: 112px' : 'padding-top: 128px'">
+        <v-container fluid>
+         <div v-for="index in 5" :key="index">
+           <Artice/>
+         </div>
+        </v-container>
+      </v-content>
     </div>
-    <router-view/>
-  </div>
+
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+// import NavDrawer from './components/nav'
+// import helloWorld from '@/components/HelloWorld';
+import Artice from '@/components/Article';
+export default {
+  name: 'App',
+  components:{
+    Artice
+  },
+  data: () => ({
+    drawer: true,
+    items: [
+      { title: 'בית', icon: 'mdi-home' }
+
+    ]
+  })
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
---
+++ src/components/Article.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-row>
+      <v-col class="mb-4">
+        <v-card outlined>
+          <v-card-title>
+             <h1 class="display-2 font-weight-bold mb-3 mx-auto">
+                   כותרת 1
+             </h1>
+          </v-card-title>
+       <v-card-text>
+         <p v-if="readMore" class="subheading font-weight-regular">
+                {{ contentToShow }}...
+        </p>
+        <p v-else class="subheading font-weight-regular">
+                {{contentToShow}}
+        </p>
+        <v-btn color="primary" @click="readMore = !readMore">
+              {{btnText}}
+          </v-btn>
+       </v-card-text>
+     
+        </v-card>
+
+      </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            content:`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras convallis elit magna, quis commodo risus dapibus at. Sed auctor, felis ac rutrum mollis, lectus sapien fringilla leo, in varius sapien dui eget elit. Curabitur at facilisis velit, eget elementum eros. Sed nec nulla faucibus, cursus ex non, ullamcorper elit. Nulla ultrices lobortis hendrerit. Aenean ultrices est at neque lacinia, at viverra risus porttitor. Donec sed malesuada lectus.
+
+                Donec ultrices ipsum non ipsum gravida, vitae bibendum nisl fermentum. Praesent dui metus, pellentesque sit amet justo in, tempor dictum lacus. Mauris id quam ante. Ut dictum, lorem et sollicitudin semper, erat nunc rhoncus nunc, et feugiat arcu ipsum et enim. Duis vitae dolor eu lacus congue iaculis. Aliquam orci sem, iaculis vel ultricies et, tincidunt non enim. Vestibulum eu maximus massa, tempus aliquet diam.
+
+                Pellentesque in leo tellus. Proin dapibus quam id venenatis eleifend. Vestibulum id enim in nisl pulvinar ullamcorper. Nunc gravida diam a consectetur vulputate. Proin laoreet eget ipsum eu lobortis. Morbi neque arcu, elementum nec urna id, ornare pharetra quam. Morbi pulvinar justo dapibus interdum rhoncus.
+
+                Mauris est neque, ultricies vel fringilla sed, ultricies quis enim. Cras diam libero, consequat sit amet facilisis id, ultricies sodales dui. Phasellus blandit semper enim, vitae hendrerit tortor bibendum sed. Proin pulvinar, ligula eget aliquet commodo, ipsum lacus ullamcorper erat, scelerisque pulvinar nunc mauris at nisl. Aliquam porta, purus et feugiat varius, est elit pellentesque dui, vel volutpat nisl mauris vitae sapien. Mauris id turpis at ligula ultricies aliquam. Donec enim nibh, ultricies vitae orci eu, lobortis facilisis eros. Suspendisse ut dolor porttitor, tristique elit eu, gravida justo. Curabitur hendrerit efficitur felis, hendrerit consectetur augue pretium tempor. Cras nisl lacus, imperdiet ut ipsum a, malesuada pretium massa. Nulla scelerisque ligula eu tincidunt rhoncus. Curabitur lacinia et libero eu accumsan.
+
+                Aliquam dapibus vestibulum viverra. Proin fringilla in odio quis cursus. Aenean iaculis non libero a dapibus. Mauris at laoreet est, a sagittis sapien. Aenean laoreet nulla eros, id condimentum dui elementum et. Nam at orci et odio tempus molestie tempor vitae tellus. Morbi ac posuere quam. Ut iaculis, magna a efficitur rhoncus, dui felis viverra urna, sit amet pellentesque velit lectus consectetur risus. Duis scelerisque nibh placerat sem auctor, at varius odio vestibulum."`,
+          readMore:true,
+        }
+    },
+    computed: {
+      btnText(){
+          return this.readMore ? `קרא עוד` : `הסתר`;
+        
+      },
+      contentToShow(){
+        return this.readMore ? this.content.slice(0,200) : this.content;
+      }
+    },
+}
+</script>
