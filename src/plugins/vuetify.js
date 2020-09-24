31f5c6b72cd2883efc639cb43384152c6e4f2bc5
Avi Twito <avi.twito1988@gmail.com>
2020-09-24 19:54:42 +0300

    Vuetify Theme Colors Change

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,5 +4,22 @@ import Vuetify from 'vuetify/lib'
 Vue.use(Vuetify)
 
 export default new Vuetify({
-  rtl: true
+  rtl: true,
+  theme:{
+    themes: {
+      light: {
+        // background: '#d9be9e',
+        // primary: '#f7c1d9',
+        primary: '#d9be9e',
+        background: '#f7dfdf',
+        accent: '#D9326B',
+        secondary: '#278FB2',
+        success: '#4CAF50',
+        info: '#2196F3',
+        warning: '#FB8C00',
+        error: '#FF5252'
+      },
+    }
+  }
+  
 })
