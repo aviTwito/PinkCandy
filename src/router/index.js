c7f6182c6b6cb67e2c517f9b744b9a8ffcbd52f1
Avi Twito <avi.twito1988@gmail.com>
2020-09-28 13:35:53 +0300

    articles cards mock data extracted to js file

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-// import ArticlePage from "@/components/ArticlePage"
+import ArticlePage from "@/components/ArticlePage";
 Vue.use(VueRouter);
 
 const routes = [
@@ -11,13 +11,14 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/article/:id",
+    name: "ArticlePage",
+    component: ArticlePage
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "@/components/ArticlePage.vue")
+    // component: () =>
+    //   import(/* webpackChunkName: "about" */ "@/components/ArticlePage.vue")
   }
 ];
 
