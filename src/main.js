85202125fdd4f2789bea4260eddce17273351999
Avi Twito <avi.twito1988@gmail.com>
2020-09-30 10:26:43 +0300

    firebase installed

--- src/main.js
+++ src/main.js
@@ -1,15 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+var firebase = require("firebase/app");
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+var firebaseConfig = {
+  apiKey: "AIzaSyDNZkOv8BmZu0qLnDs8KszicDlUDbb0rH0",
+  authDomain: "pinkcandy-88b44.firebaseapp.com",
+  databaseURL: "https://pinkcandy-88b44.firebaseio.com",
+  projectId: "pinkcandy-88b44",
+  storageBucket: "pinkcandy-88b44.appspot.com",
+  messagingSenderId: "960571149800",
+  appId: "1:960571149800:web:8415c58943fc03ffa4a85d",
+  measurementId: "G-T4EEE2T0VC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
   store,
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
