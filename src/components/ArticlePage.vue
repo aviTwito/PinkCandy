b894b67ac66ea63b693d03a8ff231e41bda05d9d
Avi Twito <avi.twito1988@gmail.com>
2020-10-06 10:28:19 +0300

    ArticlePage design fix

--- src/components/ArticlePage.vue
+++ src/components/ArticlePage.vue
@@ -1,6 +1,12 @@
 <template>
   <v-row no-gutters>
     <v-col>
+      <v-img
+        contain
+        class="white--text ma-0 pa-0"
+        max-height="400"
+        :src="selectedArticle.img"
+      />
       <v-card
         :style="$vuetify.breakpoint.smAndDown ? '' : ''"
         :class="$vuetify.breakpoint.lgAndUp ? '' : ''"
@@ -8,48 +14,66 @@
         flat
         max-width="1200"
       >
-        <v-img
+        <!-- <v-img
           contain
           class="white--text "
           max-height="400"
           :src="selectedArticle.img"
-        />
+        /> -->
         <v-card-title class="text-h5 justify-center">
           {{ selectedArticle.headline }}
         </v-card-title>
-        <v-card-text>
+        <v-card-subtitle class="text-right">
           {{ selectedArticle.description }}
-        </v-card-text>
+        </v-card-subtitle>
         <v-divider />
         <v-row dense align="start" justify="start">
           <v-col cols="sm-12">
-            <v-card-subtitle class="font-weight-bold mr-2 pa-0">
+            <v-card-subtitle class="font-weight-bold text-right mr-2 pa-0">
               מצרכים
             </v-card-subtitle>
           </v-col>
         </v-row>
 
-        <div v-for="(item, i) in selectedArticle.igredients" :key="i">
-          <v-card-text>{{ item.title }}</v-card-text>
-          <v-card-text class="mr-3 pa-0">
+        <div
+          v-for="(item, i) in selectedArticle.igredients"
+          :key="i"
+          class="mb-2"
+        >
+          <v-card-text class="text-right mb-1 pb-0 mt-0 pt-0">{{
+            item.title
+          }}</v-card-text>
+          <v-card-text class="mr-10 pa-0">
             <ul>
-              <li v-for="(subIgredient, j) in item.subIgredients" :key="j">
+              <li
+                v-for="(subIgredient, j) in item.subIgredients"
+                :key="j"
+                class="text-right"
+              >
                 {{ subIgredient }}
               </li>
             </ul>
           </v-card-text>
         </div>
         <br />
-        <v-card-subtitle class="font-weight-bold pb-1 pr-0 mr-0 pt-2">
+        <v-card-subtitle class="font-weight-bold text-right mr-2 pa-0">
           אופן הכנה
         </v-card-subtitle>
-        <div v-for="(item, i) in selectedArticle.preperation" :key="i">
-          <v-card-subtitle>
+        <div
+          v-for="(item, i) in selectedArticle.preperation"
+          :key="i"
+          class="mb-2"
+        >
+          <v-card-text class="text-right mb-1 pb-0 mt-0 pt-0">
             {{ item.title }}
-          </v-card-subtitle>
-          <v-card-text class="mr-3">
+          </v-card-text>
+          <v-card-text class="mr-10 pa-0">
             <ol>
-              <li v-for="(subItem, j) in item.steps" :key="j">
+              <li
+                v-for="(subItem, j) in item.steps"
+                :key="j"
+                class="text-right"
+              >
                 {{ subItem.text }}
                 <v-img
                   v-if="subItem.img"
